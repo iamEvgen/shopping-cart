7b1e2ddb913fdd8ee3e1b6d65134c331d3e0c110
iamEvgen <evgen.alexa@gmail.com>
2022-07-13 16:05:00 +0300

    setup react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Item from "./pages/Item";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
-    <div className="App">
-      Empty div here
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/shop/:id" element={<Item />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Item.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useParams } from "react-router-dom";
+
+function Item() {
+  const params = useParams();
+  console.log(params);
+  return <div>Item is: {params.id}</div>;
+}
+
+export default Item;
